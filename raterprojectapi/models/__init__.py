8252a73b50799036f8d5286f337cb3002ae04579
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-09 14:21:02 -0600

    added migration files and corresponding imports

---
+++ raterprojectapi/models/__init__.py
@@ -0,0 +1,7 @@
+from .category import Category
+from .game import Game
+from .gamecategory import GameCategory
+from .gamer import Gamer
+from .image import Image
+from .rating import Rating
+from .review import Review
