5f217e998cc607fb56be579e59cea8e001ad2b1c
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-21 22:16:27 -0600

    added game category but now going to try another loop. Games retrieve and List are now functioning

--- raterprojectapi/views/game.py
+++ raterprojectapi/views/game.py
@@ -10,6 +10,21 @@ from raterprojectapi.models import Game
 
 class Games(ViewSet):
 
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single event
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        try:
+            game= Game.objects.get(pk=pk)
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+
     def list(self, request):
         """Handle GET requests to games resource
 
@@ -25,6 +40,8 @@ class Games(ViewSet):
 
         serializer = GameSerializer(
                 games, many=True, context={'request': request})
+
+        game_array 
         return Response(serializer.data)
 
 class GameSerializer(serializers.ModelSerializer):
