3295a4c1fcd91d3f16a0e087b22b34eb800daa4d
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-21 22:15:18 -0600

    added related names for game and category

--- raterprojectapi/models/gamecategory.py
+++ raterprojectapi/models/gamecategory.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class GameCategory(models.Model):
 
-    game = models.ForeignKey("Game", on_delete=models.CASCADE)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name ="gamecategory")
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="gamecategory")
     
 
 
